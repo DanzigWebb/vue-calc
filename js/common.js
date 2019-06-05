1180f9aac3285c89f6c742eaf9b1e6fc4291af4a
DanzigWebb <42899726+DanzigWebb@users.noreply.github.com>
2019-06-06 00:42:09 +0300

    c

--- js/common.js
+++ js/common.js
@@ -2,20 +2,33 @@ var vue = new Vue({
   el: '#app',
   data: {
     result: '',
-    numbers: [1,2,3,4,5,6,7,8,9,0],
-    operations: ['+', '-', '*', '/'],
+    resultLive: '',
+    numbers: [1,2,3,4,5,6,7,8,9,0, '.'],
+    operations: ['+', '-', '*', '/', '^'],
 
   },
   methods:  {
     input: function(par) {
       this.result = this.result.toString();
       this.result+=par;
+      this.calcLive();
     },
     reset: function() {
       this.result = '';
     },
     calc: function() {
-      this.result = eval(this.result);
+      if (eval(this.result) == Infinity) {
+        this.deleteC();
+        alert('Делить на ноль не безопасно!');
+      } 
+      
+        else {
+        this.result = this.result.replace("^", "**");
+        this.result = eval(this.result);
+      }
+    },
+    calcLive: function() {
+      this.resultLive = eval(this.result);
     },
     deleteC: function () {
       this.result = this.result.toString();
